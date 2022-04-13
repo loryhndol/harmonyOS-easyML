c0f76465a6cecaeff67362a16e15001b990f0d75
sduzzx857 <80020410+sduzzx857@users.noreply.github.com>
2022-04-13 21:09:00 +0800

    Update pca_random.js

--- entry/src/main/js/default/pages/PCA/pca_random/pca_random.js
+++ entry/src/main/js/default/pages/PCA/pca_random/pca_random.js
@@ -2,27 +2,34 @@ import router from '@system.router';
 export default {
     data: {
         paragraphFirst: '  我们随机生成m*n维数据，第一维代表样本的个数，第二维代表每个样本的维数。',
-        m: 7,
-        n: 5,
-        after: 3,
+        m: 5,
+        n: 3,
+        after: 2,
     },
     change_x(e) {
-        this.m = 4;  //e.value
+        this.m = e.value;
     },
     change_y(e) {
-        this.n = 3;
+        this.n = e.value;
     },
     change_after(e) {
-        this.after = 2;
+        this.after = e.value;
     },
     launch() {
-        router.push ({
-            uri: 'pages/PCA/pca_out_rand/pca_out_rand',
-            params: {
-                data1: rnd(this.m, this.n),
-                after: this.after,
-            },
-        });
+        const d_after = this.after;
+        if(this.after>=this.n){
+            console.warn("降维后的维数应小于原数据维数");
+        }
+        else{
+            router.push ({
+                uri: 'pages/pca_out_rand/pca_out_rand',
+                params: {
+                    data1: rnd(this.m, this.n),
+                    after: d_after,
+                },
+            });
+        }
+
     }
 }
 
