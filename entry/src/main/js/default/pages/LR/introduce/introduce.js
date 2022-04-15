397bef80561c379bc1a47835ad56f7e70c29a9b8
ZhangJinhua <85046312+NuanBaobao@users.noreply.github.com>
2022-04-15 15:15:29 +0800

    Add files via upload

---
+++ entry/src/main/js/default/pages/LR/introduce/introduce.js
@@ -0,0 +1,15 @@
+//导入router模块，页面路由router根据页面的uri来找到目标页面，从而实现跳转
+import router from '@system.router';
+
+export default {
+    data: {
+        title: "线性回归",
+        introduce: "线性回归是利用数理统计中回归分析,来确定两种或两种以上变量间相互依赖的定量关系的一种统计分析方法" +
+        "表达形式为 y = wx+e,e为服从均值为0的正态分布的误差值"
+    },
+    launch() {
+        router.push ({
+            uri:'pages/linear_regression/linear_regression', // 指定要跳转的页面
+        })
+    }
+}
